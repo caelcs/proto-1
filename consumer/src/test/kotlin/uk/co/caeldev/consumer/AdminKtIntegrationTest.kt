223d66366d00ed667d73170a62a5c91ab4918931
cael <adolfoecs@gmail.com>
2019-02-09 22:54:42 +0000

    adding kotlin classes

---
+++ consumer/src/test/kotlin/uk/co/caeldev/consumer/AdminKtIntegrationTest.kt
@@ -0,0 +1,29 @@
+package uk.co.caeldev.consumer
+
+import io.ktor.application.Application
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.koin.test.AutoCloseKoinTest
+
+internal class AdminKtIntegrationTest: AutoCloseKoinTest() {
+
+    @Test
+    fun `health check endpoint should return success`(): Unit = withTestApplication(Application::main) {
+        with(handleRequest(HttpMethod.Get, "/health")) {
+            assertThat(response.content).isEqualTo("OK")
+            assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
+        }
+    }
+
+    @Test
+    fun `metrics endpoint should return success`(): Unit = withTestApplication(Application::main) {
+        with(handleRequest(HttpMethod.Get, "/metrics")) {
+            assertThat(response.content).isNotBlank()
+            assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
+        }
+    }
+}
---
+++ consumer/src/test/kotlin/uk/co/caeldev/consumer/MetricRegistryTest.kt
@@ -0,0 +1,35 @@
+package uk.co.caeldev.consumer
+
+import io.micrometer.core.instrument.binder.MeterBinder
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+internal class MetricRegistryTest {
+
+    @Test
+    fun `should construct and bind all meters successfully`() {
+        //Given
+        val meterBinder = mockk<MeterBinder>(relaxed = true)
+
+        //When
+        MetricRegistry(listOf(meterBinder))
+
+        //Then
+        verify { meterBinder.bindTo(any()) }
+    }
+
+    @Test
+    fun `should get all metrics available provided by meter binder`() {
+        //Given
+        val meterBinder = ClassLoaderMetrics()
+
+        //When
+        val metrics = MetricRegistry(listOf(meterBinder)).getMetrics()
+
+        //Then
+        assertThat(metrics).isNotBlank()
+    }
+}
