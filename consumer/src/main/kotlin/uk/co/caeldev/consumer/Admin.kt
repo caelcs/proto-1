05a1d32814aa21d759b261a76389c3f842700aa4
cael <adolfoecs@gmail.com>
2019-02-09 22:26:00 +0000

    adding kotlin classes

---
+++ consumer/src/main/kotlin/uk/co/caeldev/consumer/Admin.kt
@@ -0,0 +1,46 @@
+package uk.co.caeldev.consumer
+
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.micrometer.core.instrument.binder.MeterBinder
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
+import org.koin.dsl.module.module
+import org.koin.ktor.ext.inject
+
+val adminModule = module {
+    single { MetricRegistry(listOf(ClassLoaderMetrics(),
+            JvmMemoryMetrics(), JvmThreadMetrics(), ProcessorMetrics())) }
+}
+
+fun Routing.admin() {
+
+    val metricRegistry: MetricRegistry by inject()
+
+    get("/health") {
+        call.respondText("OK")
+    }
+
+    get("/metrics") {
+        call.respondText(metricRegistry.getMetrics())
+    }
+}
+
+class MetricRegistry(metrics: List<MeterBinder>) {
+
+    private val registry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+    init {
+        metrics.forEach{
+            it.bindTo(registry)
+        }
+    }
+
+    fun getMetrics(): String = registry.scrape()
+}
---
+++ consumer/src/main/kotlin/uk/co/caeldev/consumer/Application.kt
@@ -0,0 +1,38 @@
+package uk.co.caeldev.consumer
+
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.path
+import io.ktor.response.respond
+import io.ktor.routing.routing
+import org.koin.log.Logger.SLF4JLogger
+import org.koin.standalone.StandAloneContext.startKoin
+import org.slf4j.event.Level
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(ConditionalHeaders)
+    install(Compression)
+    startKoin(listOf(adminModule), logger = SLF4JLogger())
+
+    install(StatusPages) {
+        exception<NotImplementedError> { call.respond(HttpStatusCode.NotImplemented) }
+    }
+    install(CallId) {
+        generate(10)
+    }
+    install(CallLogging) {
+        level = Level.TRACE
+        callIdMdc("X-Request-ID")
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    routing {
+        admin()
+    }
+}
+
