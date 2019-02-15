f1bfa958a72366cf98acae201f0a4d004522700d
cael <adolfoecs@gmail.com>
2019-02-15 21:35:33 +0000

    final 2

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 MACHINE=`docker-machine ls | grep proto-1 | awk '{print $1}'`
 echo 'MACHINE' $MACHINE
 
+
 if [ -z "$MACHINE" ]
 then
     echo 'creating proto-1'
@@ -12,6 +13,7 @@ fi
 
 eval "$(docker-machine env proto-1)"
 export DOCKER_MACHINE_IP=`docker-machine ip proto-1`
+export TOPIC="topic-test-1"
 
 echo "Deleting volumes for zookeeper"
 rm -rf docker/zoo1
@@ -33,8 +35,8 @@ docker-compose -f docker/infrastructure.yml up -d
 sleep 5
 
 echo "creating topic"
-docker exec docker_kafka1_1 kafka-topics --create --zookeeper zoo1:2181 --replication-factor 1 --partitions 3 --topic topicTest3
-docker exec docker_kafka1_1 kafka-topics --describe --zookeeper zoo1:2181 --topic topicTest3
+docker exec docker_kafka1_1 kafka-topics --create --zookeeper zoo1:2181 --replication-factor 1 --partitions 3 --topic $TOPIC
+docker exec docker_kafka1_1 kafka-topics --describe --zookeeper zoo1:2181 --topic $TOPIC
 
 echo "running consumer and producer"
 docker-compose -f docker/infrastructure.yml -f docker/apps.yml up -d
