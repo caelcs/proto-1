59a5dc1534029358adb63d807b4c284b4fce7e71
cael <adolfoecs@gmail.com>
2019-02-15 20:26:43 +0000

    final changes

--- install.sh
+++ install.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-docker kill $(docker ps -a | grep consumer  | awk '{print $1}')
-docker kill $(docker ps -a | grep producer  | awk '{print $1}')
-docker kill $(docker ps -a | grep kafka     | awk '{print $1}')
-docker kill $(docker ps -a | grep zookeeper | awk '{print $1}')
+echo "Deleting volumes for zookeeper"
+rm -rf docker/zoo1
 
-docker volume ls -qf dangling=true | xargs docker volume rm
+echo "stopping all containers if there are running"
+docker-compose -f docker/infrastructure.yml -f docker/apps.yml down
 
-docker rm $(docker ps -a -q)
-
-docker rmi $(docker images -a | grep consumer  | awk '{print $1}')
-docker rmi $(docker images -a | grep producer  | awk '{print $1}')
-docker rmi $(docker images -a | grep kafka     | awk '{print $1}')
-docker rmi $(docker images -a | grep zookeeper | awk '{print $1}')
-
+echo "build apps"
 ./gradlew :producer:clean :producer:build
 ./gradlew :consumer:clean :consumer:build
 
+echo "build images for the apps"
 docker build -t adolfoecs/consumer:latest ./consumer
 docker build -t adolfoecs/producer:latest ./producer
 
+echo "running kafka and zookeeper"
 docker-compose -f docker/infrastructure.yml up -d
 
-docker exec docker_kafka1_1 kafka-topics --delete --zookeeper zoo1:2181 --topic topicTest3
-docker exec docker_kafka1_1 kafka-topics --create --zookeeper zoo1:2181 --replication-factor 1 --partitions 3 --topic topicTest3
+sleep 5
 
+echo "creating topic"
+docker exec docker_kafka1_1 kafka-topics --create --zookeeper zoo1:2181 --replication-factor 1 --partitions 3 --topic topicTest3
+docker exec docker_kafka1_1 kafka-topics --describe --zookeeper zoo1:2181 --topic topicTest3
+
+echo "running consumer and producer"
 docker-compose -f docker/infrastructure.yml -f docker/apps.yml up -d
