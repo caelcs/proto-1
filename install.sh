ce10ee5dc9ca7aa0aee97b3d569eea3d0793d033
cael <adolfoecs@gmail.com>
2019-02-15 13:46:27 +0000

    wip

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ docker volume ls -qf dangling=true | xargs docker volume rm
 
 docker rm $(docker ps -a -q)
 
-docker rmi $(docker images -a | grep consumer | awk '{print $1}')
-docker rmi $(docker images -a | grep producer | awk '{print $1}')
-docker rmi $(docker images -a | grep kafka    | awk '{print $1}')
-docker rmi $(docker images -a | grep zookeeper    | awk '{print $1}')
+docker rmi $(docker images -a | grep consumer  | awk '{print $1}')
+docker rmi $(docker images -a | grep producer  | awk '{print $1}')
+docker rmi $(docker images -a | grep kafka     | awk '{print $1}')
+docker rmi $(docker images -a | grep zookeeper | awk '{print $1}')
 
 ./gradlew :producer:clean :producer:build
 ./gradlew :consumer:clean :consumer:build
@@ -20,4 +20,9 @@ docker rmi $(docker images -a | grep zookeeper    | awk '{print $1}')
 docker build -t adolfoecs/consumer:latest ./consumer
 docker build -t adolfoecs/producer:latest ./producer
 
-docker-compose -f docker/platform.yml up -d
+docker-compose -f docker/infrastructure.yml up -d
+
+docker exec docker_kafka1_1 kafka-topics --delete --zookeeper zoo1:2181 --topic topicTest3
+docker exec docker_kafka1_1 kafka-topics --create --zookeeper zoo1:2181 --replication-factor 1 --partitions 3 --topic topicTest3
+
+docker-compose -f docker/infrastructure.yml -f docker/apps.yml up -d
