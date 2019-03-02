f60ef27bc5d05d42d5c83302601757f10f8d2adc
cael <adolfoecs@gmail.com>
2019-03-02 23:42:46 +0000

    wip

--- commands.sh
+++ commands.sh
@@ -6,6 +6,20 @@ create_context(){
     kubectl create --filename ./k8s/namespace.yml
 }
 
+build_apps(){
+    export CONSUMER_TOPIC="CONSUMER_TOPIC"
+    export ACK_CONSUMER_TOPIC="ACK_CONSUMER_TOPIC"
+    export ACK_CONSUMER_CLIENT_ID="clientId"
+
+    eval $(minikube docker-env)
+    ./gradlew :producer:clean :producer:build
+    ./gradlew :consumer:clean :consumer:build
+
+    echo "build images for the apps"
+    docker build -t adolfoecs/consumer:latest ./consumer
+    docker build -t adolfoecs/producer:latest ./producer
+}
+
 install_zoo(){
     echo "installing zookeeper"
 	kubectl create -n proto1 --filename ./k8s/zookeeper-deployment.yml
@@ -38,6 +52,16 @@ install_grafana(){
 	kubectl create -n proto1 -f ./k8s/grafana-service.yml
 }
 
+install_producer(){
+    echo "installing producer"
+    export CONSUMER_TOPIC="CONSUMER_TOPIC"
+    export ACK_CONSUMER_TOPIC="ACK_CONSUMER_TOPIC"
+    export ACK_CONSUMER_CLIENT_ID="clientId"
+
+	kubectl create -n proto1 -f ./k8s/producer-deployment.yml
+	kubectl create -n proto1 -f ./k8s/producer-service.yml
+}
+
 deleteAll(){
     kubectl delete services --all -n proto1
     kubectl delete deployments --all -n proto1
@@ -46,12 +70,14 @@ deleteAll(){
 }
 
 installAll(){
+    build_apps
     create_context
     install_zoo
     install_kafka
     install_node_exporter
     install_prom
     install_grafana
+    install_producer
 }
 
 $@
