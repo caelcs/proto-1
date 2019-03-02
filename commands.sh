c967dc7989b964b4e683cb61694f75640b41c174
cael <adolfoecs@gmail.com>
2019-03-02 16:46:35 +0000

    workign version of all the infrastructure

---
+++ commands.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -ex
+
+create_context(){
+    kubectl create --filename ./k8s/namespace.yml
+}
+
+create_zoo_pod(){
+	kubectl create -n proto1 --filename ./k8s/zookeeper-deployment.yml
+}
+
+create_zoo_service(){
+	kubectl create -n proto1 --filename ./k8s/zookeeper-service.yml
+}
+
+create_kafka_pod(){
+	kubectl create -n proto1 -f ./k8s/kafka-deployment.yml
+}
+
+create_kafka_service(){
+	kubectl create -n proto1 -f ./k8s/kafka-service.yml
+}
+
+create_node_exporter_pod(){
+	kubectl create -n proto1 -f ./k8s/node-exporter-deployment.yml
+}
+
+create_node_exporter_service(){
+	kubectl create -n proto1 -f ./k8s/node-exporter-service.yml
+}
+
+create_prom_config_map(){
+	kubectl create -n proto1 -f ./k8s/prom-configmap.yml
+}
+
+create_prom_pod(){
+	kubectl create -n proto1 -f ./k8s/prom-deployment.yml
+}
+
+create_prom_service(){
+	kubectl create -n proto1 -f ./k8s/prom-service.yml
+}
+
+deleteAll(){
+    kubectl delete services --all -n proto1
+    kubectl delete deployments --all -n proto1
+    kubectl delete configmaps --all -n proto1
+    kubectl delete namespace proto1
+}
+
+installAll(){
+    create_context
+    create_zoo_pod
+    create_zoo_service
+    create_kafka_pod
+    create_kafka_service
+    create_node_exporter_pod
+    create_node_exporter_service
+    create_prom_config_map
+    create_prom_pod
+    create_prom_service
+}
+
+$@
